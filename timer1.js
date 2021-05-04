e3b7bce32bbcd775e458298be74e34c64502265c
Indika Kolamba Tantrege <ikolamba2@gmail.com>
2021-05-04 18:37:56 +0000

    DRY up the code

--- timer1.js
+++ timer1.js
@@ -1,26 +1,21 @@
 let args = process.argv.splice(2);
 
 const alarm = (cmdLineArgs) => {
-  let timers = [];
-  for (let i = 0; i < cmdLineArgs.length; i++) {
-    if (!isNaN(Number(cmdLineArgs[i]))) {
-      if (cmdLineArgs[i] >= 0) {
-        timers.push(Number(cmdLineArgs[i]));
+  if (cmdLineArgs.length) {
+    for (let i = 0; i < cmdLineArgs.length; i++) {
+      let timer = cmdLineArgs[i];
+      if (!isNaN(Number(timer))) {
+        if (timer >= 0) {
+          setTimeout(() => process.stdout.write('\x07'), timer);
+        } else {
+          console.log(`Error ${timer} is negative!`);
+        }
       } else {
-        console.log(`Error ${cmdLineArgs[i]} is negative!`);
+        console.log(`Error ${timer} is not a number!`);
       }
-    } else {
-      console.log(`Error ${cmdLineArgs[i]} is not a number!`);
     }
-  }
-
-  if (!timers.length) {
-    console.log('Invalid input! Please enter only numbers');
-    return;
-  }
-
-  for (const timer of timers) {
-    setTimeout(() => process.stdout.write('\x07'), timer);
+  } else {
+    console.log('Please enter timers on command line when running the application!');
   }
 };
 
